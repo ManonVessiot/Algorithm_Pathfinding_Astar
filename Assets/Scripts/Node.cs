1548a5dc017559a321cd6b4adda06502a77f9c05
ManonVessiot <manon.vessiot@hotmail.com>
2020-08-11 16:16:51 +0200

    first version not optim

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool _walkable;
+    public int _gridPositionX;
+    public int _gridPositionY;
+
+    public int _gCost;
+    public int _hCost;
+    public int _fCost
+    {
+        get
+        {
+            return _gCost + _hCost;
+        }
+    }
+
+    public Node parent = null;
+
+    public Node(bool walkable, int gridPositionX, int gridPositionY)
+    {
+        _walkable = walkable;
+        _gridPositionX = gridPositionX;
+        _gridPositionY = gridPositionY;
+    }
+}
